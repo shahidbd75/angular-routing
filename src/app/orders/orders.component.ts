0a11f2e67ab1ce669c79714ae82c48b7888dfed4
Shahid <abdul.baset@aci-bd.com>
2021-03-21 12:28:55 +0600

    check-box-select-all

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -3,13 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-
-  constructor() { }
+  orders: Array<{
+    name: string;
+    odate: string;
+    hub: string;
+    isDelivered: boolean;
+  }>;
+  selectAllCheck;
+  constructor() {}
 
   ngOnInit(): void {
+    this.selectAllCheck = false;
+    this.orders = [
+      {
+        name: 'Miniket Gold 5KG',
+        odate: '2021-01-15',
+        hub: 'ACI Cener',
+        isDelivered: true,
+      },
+      {
+        name: 'Atta 1KG',
+        odate: '2021-01-16',
+        hub: 'ACI Cener',
+        isDelivered: false,
+      },
+      {
+        name: 'Miniket Gold 5KG',
+        odate: '2021-01-14',
+        hub: 'Novo Tower',
+        isDelivered: true,
+      },
+      {
+        name: 'Sunflower Oil 3 Liter',
+        odate: '2021-01-15',
+        hub: 'Simpletree Anarkoli',
+        isDelivered: false,
+      },
+    ];
   }
 
+  toggleAll(): void {
+    if (this.selectAllCheck) {
+      this.orders = this.orders.map((x) => {
+        return {
+          name: x.name,
+          hub: x.hub,
+          odate: x.odate,
+          isDelivered: true,
+        };
+      });
+    } else {
+      this.orders = this.orders.map((x) => {
+        return {
+          name: x.name,
+          hub: x.hub,
+          odate: x.odate,
+          isDelivered: false,
+        };
+      });
+    }
+  }
+  approved(): void {
+    console.log(this.orders);
+  }
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
-import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
-
 
 @NgModule({
   declarations: [OrdersComponent, OrderTrackingComponent],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  imports: [CommonModule, FormsModule, OrdersRoutingModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
